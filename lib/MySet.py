66beccac34dffd01f9b2eb2a3bb4bc1ad7461f6a
Abdifatah <mursalabdifatah17@gmail.com>
2025-12-05 06:06:29 +0300

    python-p3-dsa-set-codealong

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,6 +1,6 @@
 class MySet:
 
-    def __init__(self, enumerable=[]):
+    def __init__(self, enumerable = []):
         self.dictionary = {}
         for value in enumerable:
             self.dictionary[value] = True
@@ -9,11 +9,13 @@ class MySet:
         return value in self.dictionary
 
     def __str__(self):
-        set_list = [str(key) for key in self.dictionary]
-        return f"MySet: {{{', '.join(set_list)}}}"
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
 
     def add(self, value):
-        self.dictionary[value] = True
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
         return self
 
     def delete(self, value):
@@ -25,4 +27,4 @@ class MySet:
 
     def clear(self):
         self.dictionary.clear()
-        return self
+
